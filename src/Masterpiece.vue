9feb9f7f4e46ff8bae77459c08202605c7838524
ATMJin <atmjin0912@protonmail.com>
2022-05-17 23:20:39 +0800

    作品組件化

---
+++ src/Masterpiece.vue
@@ -0,0 +1,564 @@
+<template>
+  <!-- FIXME min-width->max-width -->
+  <section class="masterpiece">
+    <!-- 小圖 -->
+    <div class="cover_min_pic">
+      <div class="min_pic">
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_01.jpg"
+            alt=""
+            data-picIndex="01"
+            data-picName="masterpiece_01"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_02.jpg"
+            alt=""
+            data-picIndex="02"
+            data-picName="masterpiece_02"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_03.jpg"
+            alt=""
+            data-picIndex="03"
+            data-picName="masterpiece_03"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_04.jpg"
+            alt=""
+            data-picIndex="04"
+            data-picName="masterpiece_04"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_05.jpg"
+            alt=""
+            data-picIndex="05"
+            data-picName="masterpiece_05"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_06.jpg"
+            alt=""
+            data-picIndex="06"
+            data-picName="masterpiece_06"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_07.jpg"
+            alt=""
+            data-picIndex="07"
+            data-picName="masterpiece_07"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_08.jpg"
+            alt=""
+            data-picIndex="08"
+            data-picName="masterpiece_08"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_09.jpg"
+            alt=""
+            data-picIndex="09"
+            data-picName="masterpiece_09"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_10.jpg"
+            alt=""
+            data-picIndex="10"
+            data-picName="masterpiece_10"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_11.jpg"
+            alt=""
+            data-picIndex="11"
+            data-picName="masterpiece_11"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_12.jpg"
+            alt=""
+            data-picIndex="12"
+            data-picName="masterpiece_12"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_13.jpg"
+            alt=""
+            data-picIndex="13"
+            data-picName="masterpiece_13"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_14.jpg"
+            alt=""
+            data-picIndex="14"
+            data-picName="masterpiece_14"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_15.jpg"
+            alt=""
+            data-picIndex="15"
+            data-picName="masterpiece_15"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_16.jpg"
+            alt=""
+            data-picIndex="16"
+            data-picName="masterpiece_16"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_17.jpg"
+            alt=""
+            data-picIndex="17"
+            data-picName="masterpiece_17"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_18.jpg"
+            alt=""
+            data-picIndex="18"
+            data-picName="masterpiece_18"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_19.jpg"
+            alt=""
+            data-picIndex="19"
+            data-picName="masterpiece_19"
+          />
+        </div>
+        <div class="pic">
+          <img
+            src="./assets/img/min/masterpiece_20.jpg"
+            alt=""
+            data-picIndex="20"
+            data-picName="masterpiece_20"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="beside_min_pics">
+      <!-- 大圖和介紹文字 -->
+      <div class="card">
+        <div class="pic show">
+          <img src="./assets/img/masterpiece_01.jpg" />
+        </div>
+        <div class="pic hidden">
+          <img src="./assets/img/masterpiece_01.jpg" />
+        </div>
+
+        <div class="text show">
+          <h2>小狐狸</h2>
+          <p>
+            在處理好的布上描繪線稿後，以一針一線仔細沿著畫好的輪廓遊走。當最後都縫製好時，用力拉扯預留的線頭，使布料緊密地重疊在一起。
+          </p>
+          <p>
+            緊密重疊的部分染料不易滲透，便呈現較淺的白色，其餘部分則呈現染料的顏色。
+          </p>
+        </div>
+        <div class="text hidden">
+          <h2>小狐狸</h2>
+          <p>
+            在處理好的布上描繪線稿後，以一針一線仔細沿著畫好的輪廓遊走。當最後都縫製好時，用力拉扯預留的線頭，使布料緊密地重疊在一起。
+          </p>
+          <p>
+            緊密重疊的部分染料不易滲透，便呈現較淺的白色，其餘部分則呈現染料的顏色。
+          </p>
+        </div>
+      </div>
+      <!-- 可以連結到課程的區塊 -->
+      <aside class="cover_you_know">
+        <div class="you_know show">
+          <h3>你知道嗎?</h3>
+          <p>
+            這件作品是用<span class="skill_name">縫染</span>的技法製作而成的
+          </p>
+          <div class="link_to_other">
+            <a href="course_page.html">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-chevron-left"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+                />
+              </svg>
+              查詢相關課程
+            </a>
+            <a href="skill_page.html">
+              查詢相關技法
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-chevron-right"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div class="you_know hidden">
+          <h3>你知道嗎?</h3>
+          <p>
+            這件作品是用<span class="skill_name">縫染</span>的技法製作而成的
+          </p>
+          <div class="link_to_other">
+            <a href="course_page.html">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-chevron-left"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+                />
+              </svg>
+              查詢相關課程
+            </a>
+            <a href="skill_page.html">
+              查詢相關技法
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-chevron-right"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </aside>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+a[href="masterpiece_page.html"] {
+  border-radius: 0.25em;
+  background-color: #1a388c30;
+}
+
+main {
+  background-color: var(--masterpiece_color);
+}
+
+.masterpiece {
+  overflow: hidden;
+}
+
+.cover_min_pic {
+  padding: 15px 0;
+  width: 90%;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.min_pic {
+  height: 110px;
+  display: flex;
+  overflow-x: auto;
+  scrollbar-width: none;
+  scroll-behavior: smooth;
+  font-size: 0;
+  border-radius: 0.25rem;
+  background-color: #fff7;
+}
+
+.min_pic::-webkit-scrollbar {
+  display: none;
+}
+
+.min_pic > .pic {
+  margin: 5px;
+  flex-shrink: 0;
+  cursor: pointer;
+}
+
+.pic > img {
+  width: 100%;
+}
+
+.beside_min_pics {
+  width: 90%;
+  margin: auto;
+}
+
+.card {
+  position: relative;
+  overflow: hidden;
+  padding-bottom: 15px;
+  letter-spacing: 0.2em;
+  text-shadow: 1px 1px 2px #fff3;
+}
+
+.card > .pic {
+  width: 100%;
+}
+
+.text > h2 {
+  font-size: 2rem;
+  line-height: 2em;
+  text-align: center;
+}
+
+.text > p {
+  font-weight: 400;
+  line-height: 1.75em;
+  text-indent: 1.2em;
+  text-align: justify;
+  white-space: pre-line;
+  margin-bottom: 1em;
+}
+
+.cover_you_know {
+  position: relative;
+  padding-bottom: 20px;
+}
+
+.you_know {
+  padding: 1em;
+  width: 100%;
+  background-color: #fffa;
+  border-radius: 0.5em;
+  position: relative;
+  line-height: 2em;
+}
+
+.you_know h3::before {
+  content: "";
+  display: inline-block;
+  height: 30px;
+  width: 31px;
+  margin-right: 5px;
+  background-image: url(../img/small_logo_flower.png);
+  background-size: cover;
+  vertical-align: middle;
+}
+
+.you_know p {
+  font-size: 1rem;
+  text-align: center;
+}
+
+.link_to_other {
+  display: flex;
+  justify-content: space-around;
+}
+
+.you_know a {
+  display: inline-block;
+  color: #1a388c;
+}
+
+.you_know a:visited {
+  color: #1a388c;
+}
+
+@media screen and (min-width: 768px) {
+  .cover_min_pic {
+    padding: 20px 0;
+  }
+
+  .min_pic {
+    flex-direction: column;
+    flex-wrap: wrap;
+    height: 220px;
+  }
+
+  .beside_min_pics {
+    padding-top: 15px;
+  }
+
+  .card {
+    display: flex;
+    justify-content: space-between;
+    /*     padding: 15px 0; */
+  }
+
+  .card .pic {
+    width: 50%;
+    flex-shrink: 0;
+    display: flex;
+    align-items: center;
+  }
+
+  .card .text {
+    width: 45%;
+    flex-shrink: 0;
+  }
+}
+
+.cover_under_page {
+  background-color: var(--masterpiece_color);
+}
+
+@media screen and (max-resolution: 2.8dppx) {
+  .link_to_other {
+    font-size: 1rem;
+  }
+}
+
+@media screen and (min-width: 1200px) {
+  .masterpiece {
+    display: flex;
+    width: 80%;
+    min-width: 1200px;
+    margin: 0 auto;
+  }
+
+  .cover_min_pic {
+    width: 360px;
+    flex-shrink: 0;
+  }
+
+  .min_pic {
+    flex-direction: row;
+    flex-wrap: wrap;
+    overflow-y: auto;
+    height: 90vh;
+  }
+
+  .min_pic > .pic {
+    margin: 10px;
+  }
+
+  .beside_min_pics {
+    padding-top: 20px;
+  }
+
+  .card {
+    margin-left: auto;
+    width: 95%;
+  }
+
+  .cover_you_know {
+    margin-left: auto;
+    width: 95%;
+  }
+}
+
+@media screen and (min-width: 768px) and (min-height: 1040px) {
+  .masterpiece {
+    height: 80vh;
+  }
+}
+
+/* ============================= */
+/* 卡片滑入動畫 */
+.show {
+  position: absolute;
+  opacity: 1;
+  transition: 1s;
+}
+
+.hidden {
+  position: relative;
+  opacity: 0;
+  transition: 1s;
+}
+
+.pic.show {
+  left: 0;
+  animation-name: picshow;
+  animation-duration: 1s;
+}
+
+.text.show {
+  right: 0;
+  animation-name: textshow;
+  animation-duration: 1s;
+}
+
+.you_know.show {
+  top: 0;
+  animation-name: youknowshow;
+  animation-duration: 1s;
+}
+
+@keyframes picshow {
+  0% {
+    opacity: 0;
+    left: -500px;
+  }
+
+  100% {
+    opacity: 1;
+    left: 0;
+  }
+}
+
+@keyframes textshow {
+  0% {
+    opacity: 0;
+    right: -500px;
+  }
+
+  100% {
+    opacity: 1;
+    right: 0;
+  }
+}
+
+@keyframes youknowshow {
+  0% {
+    opacity: 0;
+    top: 300px;
+  }
+
+  100% {
+    opacity: 1;
+    top: 0;
+  }
+}
+</style>
