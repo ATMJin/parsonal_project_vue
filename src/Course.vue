9e9ccbb058363edba0c4ea8de1302d41e99f80f7
ATMJin <atmjin0912@protonmail.com>
2022-05-17 23:04:12 +0800

    課程頁組件化

---
+++ src/Course.vue
@@ -0,0 +1,517 @@
+<template>
+  <section class="course">
+    <div class="container">
+      <div class="row">
+        <div class="col_12 col_md_4 col_xl_3">
+          <div class="card">
+            <div class="pic">
+              <img src="./assets/img/course_01.jpg" />
+            </div>
+            <div class="text">
+              <div>
+                <h2>蠟染</h2>
+                <div class="course_info">
+                  <div class="course_people">開課人數: <span>10</span> 人</div>
+                  <div class="course_item">自備器材: <span>否</span></div>
+                </div>
+                <div class="course_introduce">
+                  <p>
+                    可以隨心所欲地繪製想要的圖案。
+                    我們有提供相關材料，您只需要攜帶畫筆和一顆想要創作的心前來就可以了。
+                  </p>
+                </div>
+              </div>
+
+              <div>
+                <div class="for_beginner"><span>初學者推薦課程</span></div>
+                <div class="course_money">
+                  <span>課程費用: </span><span>1200</span><span>元</span>
+                </div>
+                <div class="about_others">
+                  <a href="masterpiece_page.html">
+                    <i class="bi bi-arrow-left"></i><span>相關作品</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="out_ribbon">
+              <div class="ribbon"></div>
+            </div>
+          </div>
+        </div>
+        <div class="col_12 col_md_4 col_xl_3">
+          <div class="card">
+            <div class="pic">
+              <img src="./assets/img/course_02.jpg" />
+            </div>
+            <div class="text">
+              <div>
+                <h2>卷上絞技巧</h2>
+                <div class="course_info">
+                  <div class="course_people">開課人數: <span>10</span> 人</div>
+                  <div class="course_item">自備器材: <span>否</span></div>
+                </div>
+                <div class="course_introduce">
+                  <p>
+                    纏繞纏繞纏繞，然後絞緊。
+                    利用繩線與布料產生的皺褶，能產生相當獨特的花紋。
+                  </p>
+                </div>
+              </div>
+
+              <div>
+                <div class="not_beginner"><span> </span></div>
+                <div class="course_money">
+                  <span>課程費用: </span><span>1200</span><span>元</span>
+                </div>
+                <div class="about_others">
+                  <a href="masterpiece_page.html">
+                    <i class="bi bi-arrow-left"></i><span>相關作品</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col_12 col_md_4 col_xl_3">
+          <div class="card">
+            <div class="pic">
+              <img src="./assets/img/course_03.jpg" />
+            </div>
+            <div class="text">
+              <div>
+                <h2>軍隊縫</h2>
+                <div class="course_info">
+                  <div class="course_people">開課人數: <span>10</span> 人</div>
+                  <div class="course_item">自備器材: <span>否</span></div>
+                </div>
+                <div class="course_introduce">
+                  <p>
+                    如軍隊般整整齊齊地縫製。
+                    整齊的縫製紋與由細小紋路形成的圖案是其特點。
+                  </p>
+                </div>
+              </div>
+              <div>
+                <div class="not_beginner"><span> </span></div>
+                <div class="course_money">
+                  <span>課程費用: </span><span>1200</span><span>元</span>
+                </div>
+                <div class="about_others">
+                  <a href="masterpiece_page.html">
+                    <i class="bi bi-arrow-left"></i><span>相關作品</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col_12 col_md_4 col_xl_3">
+          <div class="card">
+            <div class="pic">
+              <img src="./assets/img/course_04.jpg" />
+            </div>
+            <div class="text">
+              <div>
+                <h2>帽子絞</h2>
+                <div class="course_info">
+                  <div class="course_people">開課人數: <span>10</span> 人</div>
+                  <div class="course_item">自備器材: <span>否</span></div>
+                </div>
+                <div class="course_introduce">
+                  <p>
+                    想要製作一頂專屬於自己的帽子嗎?
+                    帽子絞技法可以讓您走在路上時成為眾人注目的焦點。
+                  </p>
+                </div>
+              </div>
+              <div>
+                <div class="not_beginner"><span> </span></div>
+                <div class="course_money">
+                  <span>課程費用: </span><span>1200</span><span>元</span>
+                </div>
+                <div class="about_others">
+                  <a href="masterpiece_page.html">
+                    <i class="bi bi-arrow-left"></i><span>相關作品</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col_12 col_md_4 col_xl_3">
+          <div class="card">
+            <div class="pic">
+              <img src="./assets/img/course_05.jpg" />
+            </div>
+            <div class="text">
+              <div>
+                <h2>日出絞</h2>
+                <div class="course_info">
+                  <div class="course_people">開課人數: <span>10</span> 人</div>
+                  <div class="course_item">自備器材: <span>否</span></div>
+                </div>
+                <div class="course_introduce">
+                  <p>如旭日東昇般的紋路。 適合創作廣闊意象的創作。</p>
+                </div>
+              </div>
+              <div>
+                <div class="not_beginner"><span> </span></div>
+                <div class="course_money">
+                  <span>課程費用: </span><span>1200</span><span>元</span>
+                </div>
+                <div class="about_others">
+                  <a href="masterpiece_page.html">
+                    <i class="bi bi-arrow-left"></i><span>相關作品</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col_12 col_md_4 col_xl_3">
+          <div class="card">
+            <div class="pic">
+              <img src="./assets/img/course_06.jpg" />
+            </div>
+            <div class="text">
+              <div>
+                <h2>水管紋</h2>
+                <div class="course_info">
+                  <div class="course_people">開課人數: <span>10</span> 人</div>
+                  <div class="course_item">自備器材: <span>否</span></div>
+                </div>
+                <div class="course_introduce">
+                  <p>
+                    讓您家的水管除了打小孩外還能有其他用途。
+                    平行、螺旋、交錯，在拆線前不知道會產生什麼圖案。
+                  </p>
+                </div>
+              </div>
+              <div>
+                <div class="not_beginner"><span> </span></div>
+                <div class="course_money">
+                  <span>課程費用: </span><span>1200</span><span>元</span>
+                </div>
+                <div class="about_others">
+                  <a href="masterpiece_page.html">
+                    <i class="bi bi-arrow-left"></i><span>相關作品</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- <div class="row "> -->
+
+      <!-- </div> -->
+    </div>
+
+    <aside class="pic_3d">
+      <div class="d3_position">
+        <button id="pre"><i class="bi bi-chevron-compact-left"></i></button>
+        <div class="out_3d">
+          <div class="middle_3d">
+            <div style="--i: 0" class="inner_3d">
+              <img src="./assets/img/course_01.jpg" alt="" />
+            </div>
+            <div style="--i: 1" class="inner_3d">
+              <img src="./assets/img/course_02.jpg" alt="" />
+            </div>
+            <div style="--i: 2" class="inner_3d">
+              <img src="./assets/img/course_03.jpg" alt="" />
+            </div>
+            <div style="--i: 3" class="inner_3d">
+              <img src="./assets/img/course_04.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+        <button id="next"><i class="bi bi-chevron-compact-right"></i></button>
+      </div>
+      <div class="close_3d"></div>
+    </aside>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css");
+
+a[href="course_page.html"] {
+  border-radius: 0.25em;
+  background-color: #1a388c30;
+}
+
+.course {
+  background-color: var(--course_color);
+  padding-top: 10px;
+}
+
+.card {
+  background: #fff6;
+  height: calc(100% - 10px);
+  padding: 10px;
+  border-radius: 0.25em;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+.pic {
+  > img {
+    width: 100%;
+    cursor: pointer;
+    box-shadow: 2px 2px 2px 0px #000c;
+    transition: 0.5s;
+
+    &:hover {
+      transform: scale(1.05);
+      box-shadow: 4px 4px 4px 2px #0009;
+    }
+  }
+}
+
+.text {
+  font-size: 1rem;
+  font-weight: 300;
+  line-height: 1.5;
+  letter-spacing: 1px;
+  text-align: justify;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.course_info {
+  display: flex;
+}
+
+.course_people {
+  flex-grow: 0.5;
+}
+
+.course_item {
+  flex-grow: 1;
+}
+
+.course h2 {
+  margin-top: 0.5rem;
+  line-height: 2;
+  font-size: 1.25rem;
+  font-weight: 500;
+}
+
+.course_introduce {
+  text-indent: 1.2em;
+  padding: 1em 0;
+  white-space: pre-line;
+}
+
+.not_beginner {
+  white-space: pre;
+  margin: 10px;
+}
+
+.course .for_beginner {
+  text-align: center;
+  margin: 10px;
+}
+
+.for_beginner span {
+  outline: 2px blue solid;
+  padding: 2px 4px;
+  border-radius: 5px;
+}
+
+.course_money {
+  text-align: right;
+  font-size: 1.15em;
+  font-weight: 400;
+}
+
+.about_others * {
+  color: #1a388c;
+  vertical-align: middle;
+}
+
+/* 3D輪播 */
+.pic_3d {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 100vw;
+  height: 45vh;
+  margin: auto;
+  background: #fffc;
+  visibility: hidden;
+  z-index: -10;
+}
+
+.d3_position {
+  position: relative;
+  display: flex;
+  justify-content: center;
+}
+
+.out_3d {
+  width: 70%;
+  perspective-origin: center;
+  perspective: 1500px;
+}
+
+.middle_3d {
+  width: 100%;
+  /* height: 100%; */
+  transform-style: preserve-3d;
+  /* transform: rotateX(0deg) rotateY(0deg) rotateZ(0deg); */
+  transition: 1s cubic-bezier(0.29, 0.84, 0.32, 1.34);
+}
+
+.inner_3d {
+  width: 100%;
+  position: absolute;
+  transform-style: preserve-3d;
+  transform: rotateY(calc(var(--i) * 90deg)) translateZ(100px);
+  /* transform: rotateX(0deg) rotateY(0deg) rotateZ(0deg); */
+}
+
+.pic_3d button {
+  position: absolute;
+  z-index: 1;
+  top: 210px;
+  cursor: pointer;
+  font-size: 50px;
+  font-weight: 800;
+  border: none;
+  border-radius: 0.5em;
+  background-color: #0004;
+}
+
+#pre {
+  left: 0;
+  /* margin-right: 2%; */
+}
+
+#next {
+  right: 0;
+  /* margin-left: 2%; */
+}
+
+.close_3d {
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background: #fff;
+  cursor: pointer;
+  transform: rotate(45deg);
+  transition: 1s;
+
+  &:hover {
+    transform: rotate(225deg) scale(1.25);
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 5px;
+    left: 24px;
+    width: 2px;
+    height: 40px;
+    background: #000;
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 24px;
+    left: 5px;
+    width: 40px;
+    height: 2px;
+    background: #000;
+  }
+}
+
+@media screen and (min-width: 768px) {
+  .text {
+    h2 {
+      font-size: 1.5rem;
+    }
+    :not(h2) {
+      font-size: 1.25rem;
+    }
+  }
+
+  .course_info div {
+    font-size: 1rem;
+  }
+
+  .pic_3d {
+    height: 50vh;
+  }
+
+  .out_3d {
+    width: 60%;
+  }
+}
+
+@media screen and (min-width: 992px) {
+  .pic_3d {
+    height: 100vh;
+  }
+
+  .out_3d {
+    width: 40%;
+  }
+}
+
+/* card裝飾 */
+.out_ribbon {
+  position: absolute;
+  height: 100px;
+  width: 100px;
+  top: -7px;
+  left: -7px;
+  overflow: hidden;
+
+  &::before {
+    content: "";
+    height: 0px;
+    width: 0px;
+    border-bottom: 7px solid black;
+    border-left: 7px solid #fff0;
+    position: absolute;
+    left: 47px;
+  }
+
+  &::after {
+    content: "";
+    height: 0px;
+    width: 0px;
+    border-bottom: 7px solid #000;
+    border-left: 7px solid #fff0;
+    position: absolute;
+    top: 47px;
+  }
+}
+
+.ribbon {
+  position: absolute;
+  width: 100px;
+  height: 15px;
+  left: -30px;
+  top: 15px;
+  background: #f00;
+  transform: rotate(-45deg);
+}
+
+/* ============================== */
+.cover_under_page {
+  background-color: var(--course_color);
+}
+</style>
