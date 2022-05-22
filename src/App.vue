1464aef0655f0007b11a25e8314a382bdd7e205a
ATMJin <atmjin0912@protonmail.com>
2022-05-22 12:50:52 +0800

    刪除App的Vue預設CSS

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,6 @@ import WebFooter from "./components/WebFooter.vue";
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 * {
   box-sizing: border-box;
 }
